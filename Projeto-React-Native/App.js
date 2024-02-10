8491d062196036bd4ca6e6c641f0db5c4fbc142c
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-09 22:43:00 -0300

    criando app android

---
+++ Projeto-React-Native/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.teste}>Open up App.js to start working on your app!</Text>
+      <Button
+        title="Learn More"
+        onPress={() => Alert.alert('Cannot press this one')}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  teste: {
+    color: '#fff',
+  }
+});
